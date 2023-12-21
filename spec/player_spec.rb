769926b58b5b94405e0986ab4cff099047ce0064
Nic Peterson <23559376+nic-peterson@users.noreply.github.com>
2023-12-21 15:20:51 -0800

    Completed Player class.

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,1 +1,15 @@
-require_relative '../lib/player.rb '
+require_relative '../lib/player.rb'
+
+describe Player do
+  subject(:player) { described_class.new('Alice', 'X') }
+
+  describe '#initialize' do
+    it 'creates a player with a name' do
+      expect(player.name).to eq('Alice')
+    end
+
+    it 'creates a player with a symbol' do
+      expect(player.symbol).to eq('X')
+    end
+  end
+end
